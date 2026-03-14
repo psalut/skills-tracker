5e8760e8542e3f4a8bd2e9d0dea1541743571c20
Pablo Salut <pablosalut03@gmail.com>
2026-03-14 08:47:59 -0300

    feat(auth): :sparkles: implement authentication flow with JWT interceptor and route guards

--- apps/web/src/app/app.config.ts
+++ apps/web/src/app/app.config.ts
@@ -2,9 +2,15 @@ import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
 } from '@angular/core';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { authInterceptor } from './core/interceptor/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(appRoutes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(appRoutes),
+    provideHttpClient(withInterceptors([authInterceptor]))
+  ],
 };
